85256cbb667eaa13495f5e17a31650b913dab4fe
Emile Ivan MINANG <ivanminang@gmail.com>
2023-04-27 18:36:58 -0400

    provisioning aws vpc with terraform

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Project Building Vpc on AWS using Terraform
+# Terraform Block 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+# Configure the aws Provider
+provider "aws" {
+  region = "us-west-2"
+}
+# create a Vpc
+resource "aws_vpc" "my-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
